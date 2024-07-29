dd9607f134e8c767b3d0aa764ef9a477dc9fa194
Syeda Hoorain Ali <syedahoorainali8gmail.com>
2024-07-29 22:21:57 +0500

    project 05-word-counter

---
+++ 05-word-counter/README.md
@@ -0,0 +1,25 @@
+# Word Counter CLI
+
+Word Counter CLI is a simple command-line tool built with Node.js for quickly counting words in sentences.
+
+## Features
+
+- Enter any sentence to get the word count instantly.
+- Streamlined interface for efficient word counting tasks.
+- Colorful output for enhanced readability.
+
+## Usage
+
+- Run the application: `npx @syedahoorainali/word-counter`
+- Input your sentence when prompted.
+- Press `Enter` to see the word count displayed.
+
+## Technologies Used
+
+- Node.js
+- inquirer
+- chalk
+
+## Contributors
+
+- [Syeda Hoorain Ali](https://github.com/syeda-hoorain-ali)
---
+++ 05-word-counter/index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+//* Function to prompt user for input sentence
+const promptUserForSentence  = async () => {
+    const { inputSentence } = await inquirer.prompt({
+        message: chalk.bold.yellowBright("Enter the sentence to count words. "),
+        type: "input",
+        name: "inputSentence",
+    })
+    return inputSentence;
+}
+
+//* Function to count words in a given text
+const countWords = (text: string) => {
+    const wordsArray = text.trim().split(' '); // Split the text into words
+    const nonEmptyWordsArray = wordsArray.filter(word => word.length > 0) // Filter out empty strings
+    return nonEmptyWordsArray.length;
+}
+
+//* Main function to orchestrate the word counting process
+const main = async () => {
+    console.log(`     ********** ${chalk.greenBright.bold("Word Counter")} **********`);
+    const sentence = await promptUserForSentence ();
+    const words = countWords(sentence);
+    console.log(chalk.cyanBright(`Total words in your sentences are ${chalk.magentaBright(words)}`));
+}
+
+//* Execute the main function
+await main();
+
